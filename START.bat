89f8d1c0c57ed606a17d8f55af942304319bf82d
Buruska <buruska97@gmail.com>
2019-06-10 20:34:49 +0200

    START commit

--- START.bat
+++ START.bat
@@ -3,14 +3,8 @@ git clone https://github.com/buruska97/OffoschMinecraft.git .
 git remote add origin https://github.com/buruska97/OffoschMinecraft.git
 git pull https://github.com/buruska97/OffoschMinecraft.git
 git reset --hard
-echo "Repository cloned"
-echo "SERVER RUN FINISHED"
-pause
-echo "PARENT DIRECTORY CHOSEN"
-pause
+RUN
 git add *
-pause
 git commit -a -m "autoupdate %date%-%time%"
-pause
-git push --set-upstream origin master
+git push origin master
 pause

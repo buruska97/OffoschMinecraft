8b0b193c92971dba9228b9f0ab750832928bc86f
Buruska <buruska97@gmail.com>
2019-06-10 21:00:25 +0200

    Manual commit 3

--- START.bat
+++ START.bat
@@ -3,7 +3,7 @@ git clone https://github.com/buruska97/OffoschMinecraft.git .
 git remote add origin https://github.com/buruska97/OffoschMinecraft.git
 git pull https://github.com/buruska97/OffoschMinecraft.git
 git reset --hard
-RUN
+
 git add *
 git rm --cached START.bat
 git commit -a -m "autoupdate %date%-%time%"
